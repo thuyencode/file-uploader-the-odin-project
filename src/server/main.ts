4cad8bf3ad30fce1002cb9439e8ce86672593ae3
Thuyen Code <thuyencode@proton.me>
2024-11-12 19:19:35 +0700

    Reformat all files with Prettier

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import ViteExpress from "vite-express";
+import express from 'express'
+import ViteExpress from 'vite-express'
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
-const app = express();
+const app = express()
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
-});
+app.get('/hello', (_, res) => {
+  res.send('Hello Vite + React + TypeScript!')
+})
 
 ViteExpress.listen(app, PORT, () =>
   console.log(`Server is listening on port ${PORT}...`)
-);
+)
--- src/server/test.ts
+++ src/server/test.ts
@@ -1,3 +1,3 @@
 export const sayHere = () => {
-  console.log("Here");
-};
+  console.log('Here')
+}
