69e2df9ba2f9e5484bbbb177737160e7a0b975a7
Thuyen Code <thuyencode@proton.me>
2024-11-12 19:36:29 +0700

    Add ESLint's configs

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import js from '@eslint/js'
+import pluginQuery from '@tanstack/eslint-plugin-query'
+import pluginRouter from '@tanstack/eslint-plugin-router'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+import ts_eslint from 'typescript-eslint'
+
+export default ts_eslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...ts_eslint.configs.recommended,
+      ...pluginQuery.configs['flat/recommended'],
+      ...pluginRouter.configs['flat/recommended'],
+      eslintConfigPrettier
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'jsx-a11y': jsxA11y,
+      react
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true }
+      ],
+      '@tanstack/query/exhaustive-deps': 'error'
+    }
+  }
+)
