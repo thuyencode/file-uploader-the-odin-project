f9647e9046451a87e70729e3122fcb0d992f69ce
Thuyen Code <thuyencode@proton.me>
2024-11-14 10:43:53 +0700

    Add needed middlewares

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,10 +1,26 @@
+import compression from 'compression'
+import cookieParser from 'cookie-parser'
 import e from 'express'
+import helmet from 'helmet'
 import ViteExpress from 'vite-express'
 import { env } from './utils/env'
 
 const PORT = env.PORT
 
 const app = e()
+
+app.use(compression())
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false
+  })
+)
+
+app.use(e.json())
+app.use(e.urlencoded({ extended: true }))
+
+app.use(cookieParser(env.COOKIE_SECRET_KEY))
 
 app.get('/hello', (_, res) => {
   res.send('Hello Vite + React + TypeScript!')
