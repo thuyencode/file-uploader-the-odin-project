abe5337ee930b1d423e74425bffa3d3b16f90f9d
Thuyen Code <thuyencode@proton.me>
2024-11-15 09:59:58 +0700

    Add methods to interact with the `User` table

--- src/server/db/user.db.ts
+++ src/server/db/user.db.ts
@@ -1,12 +1,24 @@
 import type { User } from '@prisma/client'
 import prisma from './client'
 
-export const findUserByUsername = async (username: string) => {
+const findByUsername = async (username: string) => {
   return await prisma.user.findUnique({ where: { username } })
 }
 
-export const findUserById = async (id: string) => {
+const findById = async (id: string) => {
   return await prisma.user.findUnique({ where: { id } })
 }
 
+type InsertNewUserProps = Omit<User, 'id' | 'created_date' | 'updated_date'>
+
+const insert = async (newUser: InsertNewUserProps) => {
+  return await prisma.user.create({ data: newUser })
+}
+
 export type { User }
+
+export const UserDB = {
+  findByUsername,
+  findById,
+  insert
+}
