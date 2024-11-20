509a2843412578b49757482ac810405857965952
Thuyen Code <thuyencode@proton.me>
2024-11-20 10:25:54 +0700

    Install DaisyUI and add some Tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,29 @@
-/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+import daisyuiThemes from 'daisyui/src/theming/themes'
+import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      container: { center: true },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
-  plugins: []
-}
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...daisyuiThemes.winter
+        },
+        dark: {
+          ...daisyuiThemes.night
+        }
+      }
+    ]
+  }
+} satisfies Config
