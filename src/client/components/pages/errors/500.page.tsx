2e707d88de7561080ae48ca0a20767c082d2e738
Thuyen Code <thuyencode@proton.me>
2024-12-03 14:43:30 +0700

    Fix import paths

--- src/client/components/pages/errors/500.page.tsx
+++ src/client/components/pages/errors/500.page.tsx
@@ -1,4 +1,4 @@
-import { Icon } from '@iconify/react/dist/iconify.js'
+import { Icon } from '@iconify/react'
 import { Link } from '@tanstack/react-router'
 import type { ReactElement } from 'react'
 
